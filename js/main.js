85c00d61fed2056b8786073e842302125325a71c
sema <semamihailov05@gmail.com>
2025-09-29 20:35:25 +0300

    save

--- js/main.js
+++ js/main.js
@@ -18,33 +18,46 @@ $(function () {
     });
     
     // carousels init
-    // let worksCarousel = $('.owl-carousel.works-carousel');
-    // worksCarousel.owlCarousel({
-    //     loop: true,
-    //     margin: 24,
-    //     nav: false,
-    //     dots: false,
-    //     responsive: {
-    //         0: {
-    //             items: 1.2,
-    //             margin: 10,
-    //             center: true,
-    //         },
-    //         480: {
-    //             items: 2,
-    //         },
-    //         1024: {
-    //             items: 4,
-    //         }
-    //     }
-    // })
-    // $(".works-carousel-next").on("click", function () {
-    //     worksCarousel.trigger('next.owl.carousel', [600]);
-    // });
-    // $(".works-carousel-prev").on("click", function () {
-    //     worksCarousel.trigger('prev.owl.carousel', [600]);
-    // });
+    let servicesCarousel = $('.owl-carousel.services-carousel');
+    servicesCarousel.owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 10,
+                center: true,
+            },
+            1024: {
+                items: 3,
+            }
+        }
+    })
+    $(".services-carousel-next").on("click", function () {
+        servicesCarousel.trigger('next.owl.carousel', [600]);
+    });
+    $(".services-carousel-prev").on("click", function () {
+        servicesCarousel.trigger('prev.owl.carousel', [600]);
+    });
 
+
+    // services tab logic
+    $(".services-tab").on("click", function () {
+        let index = $(this).index(); // получаем номер таба (0,1,2,3)
+
+        // Убираем активный класс у всех табов и добавляем только на текущий
+        $(".services-tab").removeClass("active");
+        $(this).addClass("active");
+
+        // Прячем все контейнеры и показываем соответствующий
+        $(".services-carousel-container").hide();
+        $(".services-carousel-container").eq(index).fadeIn(300); // плавное появление
+    });
+
+    // Изначально показываем только первый контейнер
+    $(".services-carousel-container").hide().first().show();
 
 
 })
